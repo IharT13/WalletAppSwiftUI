6219bdd65acb140ebf975cba23c25785e85002de
Ihar Tsimafeyeu <ColdSun1395@gmail.com>
2022-08-29 23:51:51 +0300

    initial commit

---
+++ WalletAppSwiftUI/WalletAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppSwiftUIApp.swift
+//  WalletAppSwiftUI
+//
+//  Created by Игорь Тимофеев on 29.08.22.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
